6fb47e496f45cfd792bbef0c121563e0f092f43d
Vlad <vladov3000@gmail.com>
2017-07-06 19:06:39 -0700

    WUZZAP

--- goats.py
+++ goats.py
@@ -7,9 +7,9 @@ win=0
 
 for i in range(0, 6):
     for ii in range(0, 6):
-        if i > ii:
+        if dice1[i] > dice2[ii]:
             win += 1
 
-s=str(round(win/30.0 , 5))
+s=str(round(win/36.0 , 5))
 s += '0'*(7-len(s))
 print s
