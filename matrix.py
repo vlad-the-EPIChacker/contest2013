692a4d7454239dc798152c20ed32ad8da202512c
Vlad <vladov3000@gmail.com>
2017-09-05 20:41:50 -0700

    WUZZAP

--- matrix.py
+++ matrix.py
@@ -1,20 +1,22 @@
 def det(col,row):
     sum=0
     if len(col)==2:
+        #print "2x2",m[row[0]][col[0]], m[row[1]][col[1]],m[row[0]][col[1]],m[row[1]][col[0]]
         return m[row[0]][col[0]]*m[row[1]][col[1]]-m[row[0]][col[1]]*m[row[1]][col[0]]
     x=0
-    if len(col)==3:
-        None
-    row1 = [row[ii] for ii in range(0, len(row)) if ii != x]
     for y in range(0,len(col)):
-        col1=[col[ii] for ii in range(0, len(col)) if ii!=y]
-        sum1 = det(col1, row1)
-        co=m[x][y]
-        if col[y]%2==1:
+        row1=[row[ii] for ii in range(0, len(row)) if ii!=x]
+        col1=[row[ii] for ii in range(0, len(col)) if ii!=y]
+        sum1=det(col1,row1)
+        co=m[row[x]][col[y]]
+        if y%2==1:
             co *= -1
+        if len(col)==4:
+            print "3x3",sum1,co
+        if len(col)==3:
+            print sum1,co
         sum1 *= co
         sum += sum1
-    print sum
     return sum
 
 
